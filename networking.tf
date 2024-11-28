c89539ec71a867eab9f053aef2bf676d6e052f76
susanadavila0 <susanadavilacarmona@gmail.com>
2024-11-28 15:27:01 -0800

    Create networking.tf

---
+++ networking.tf
@@ -0,0 +1,40 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_subnet" "main_a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "main_b" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+}
+
+resource "aws_route_table_association" "main_a" {
+  subnet_id      = aws_subnet.main_a.id
+  route_table_id = aws_route_table.main.id
+}
+
+resource "aws_route_table_association" "main_b" {
+  subnet_id      = aws_subnet.main_b.id
+  route_table_id = aws_route_table.main.id
+}
